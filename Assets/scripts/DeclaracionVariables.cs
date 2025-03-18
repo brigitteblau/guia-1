6af9b003a792405003943cbec11d4a208fcb182d
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-03-18 14:24:46 -0300

    declaracion de variables
    :)

---
+++ Assets/scripts/DeclaracionVariables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionVariables : MonoBehaviour
+{
+    string nombre = brigu;
+    int cantidadAlumni = 1;
+    float kilosVendidos = 1.6;
+    bool tieneComision = true; 
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
