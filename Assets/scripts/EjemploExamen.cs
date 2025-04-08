d6e44b48a61502369ba5dbdb238af653e738ca6d
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-04-08 09:03:26 -0300

    ejercicio modelo de prueba

---
+++ Assets/scripts/EjemploExamen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploExamen : MonoBehaviour
+{
+
+    public float num1;
+    public string letra ;
+    float montoMinimo = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (num1 < montoMinimo)
+        {
+            Debug.Log("el monto minimo es 1000");
+            return;
+        }
+            if (letra == "D")
+            {
+                Debug.Log(num1 + "  pesos es igual a " + (num1 / 1300) + " dolares");
+            }
+            else if (letra == "E")
+            {
+                Debug.Log(num1 +  "  pesos es igual a" + (num1 / 1500) + " euros");
+            }
+            else if (letra == "R")
+            {
+                Debug.Log(num1 + "pesos es igual a" + (num1 / 500) + "reales");
+            }
+            else
+            {
+                Debug.Log("ingresa una letra apropiada D, E o R");
+           
+            }
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/EjemploExamen2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploExamen2 : MonoBehaviour
+{
+
+    public string nombreAlumno;
+    public int añoAlumno;
+    public string espAlumno;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( nombreAlumno == null || añoAlumno < 6 && añoAlumno > 0)
+        {
+            Debug.Log("error, ingresa datos validos");
+            return;
+        }
+        else 
+        {
+            Debug.Log("mmm");
+        }
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
