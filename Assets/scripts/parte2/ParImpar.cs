210940ef34c2dc73208d2ab5ca698d5d8ae7a2a8
userbrigu <brigitteyaelblau@gmail.com>
2025-04-07 21:37:43 -0300

    hasta el ejercicio 6 terminado

---
+++ Assets/scripts/parte2/ParImpar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParImpar : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1 % 2 != 0){
+            Debug.Log("el numero ingrasado es impar");
+        }
+        else {
+            Debug.Log("el numero  ingresado es par");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/parte2/SumaResultado.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumaResultado : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("la suma entre " + num1 + " y " + num2 + "da como resultado " + (num1 + num2 ));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
